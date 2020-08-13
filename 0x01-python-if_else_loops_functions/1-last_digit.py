c68b7bdb5ed318aa960fa22dc0b1d34bad6003d4
JwowSquared <Jdwagner1@hotmail.com>
2020-08-13 03:37:07 +0000

    now checks if number is negative to have the negative persist

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 descriptor = "Last digit of {:d} is {:d} and is "
+mod = 10
 
-if number % 10 > 5:
+if number < 0:
+    mod = -10
+
+if number % mod > 5:
     descriptor += "greater than 5"
-elif number % 10 == 0:
+elif number % mod == 0:
     descriptor += "0"
 else:
     descriptor += "less than 6 and not 0"
 
-print(descriptor.format(number, number % 10))
+print(descriptor.format(number, number % mod))
