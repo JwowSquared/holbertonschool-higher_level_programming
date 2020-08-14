a14257fddde06d8cad4d1dd9142ffb8de6a330e2
JwowSquared <Jdwagner1@hotmail.com>
2020-08-14 09:08:24 +0000

    accounts for an empty string now

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,7 @@
 
 
 def multiple_returns(sentence):
-    if sentence is None:
+    i = len(sentence)
+    if sentence is None or i == 0:
         return (0, None)
-    return (len(sentence), sentence[0])
+    return (i, sentence[0])
