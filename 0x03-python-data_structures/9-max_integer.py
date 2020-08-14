1fbc39a5f100aca8ee9d52c08f5a967100930466
JwowSquared <Jdwagner1@hotmail.com>
2020-08-14 09:17:22 +0000

    fixed pep8

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,5 +7,5 @@ def max_integer(my_list=[]):
     m = my_list[0]
     for i in range(1, len(my_list)):
         if m < my_list[i]:
-           m = my_list[i]
+            m = my_list[i]
     return m
