a0a173829df7ea3756ff5ad61ace46368887c452
JwowSquared <Jdwagner1@hotmail.com>
2020-08-15 20:06:58 +0000

    added task 12 script

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    romans = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    vals = []
+    top = 1
+    total = 0
+    for c in reversed(roman_string):
+        if romans[c] < top:
+            total += romans[c] * -1
+        else:
+            top = romans[c]
+            total += top
+    return total
