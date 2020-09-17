3b7f780283c1a01c7f0a76d899d5ad010be1f296
JwowSquared <Jdwagner1@hotmail.com>
2020-09-17 22:27:15 +0000

    fixed pep8 and added return to error case

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,7 @@
 def safe_function(fct, *args):
     import sys
     try:
-       return fct(args[0], args[1])
+        return fct(args[0], args[1])
     except BaseException as err:
-       print("Exception: {}".format(err), file=sys.stderr)
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
