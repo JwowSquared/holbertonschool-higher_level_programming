ad1615d007337047b2fe7593083d192a948edf39
JwowSquared <Jdwagner1@hotmail.com>
2020-09-18 03:26:47 +0000

    added task 1 script

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module is an evolution of the Square Class"""
+
+
+class Square:
+    """Square Class with size attribute"""
+    def __init__(self, size):
+        """Basic Constructor"""
+        self.__size = size
