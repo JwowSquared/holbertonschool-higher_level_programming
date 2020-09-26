96aee4132938147734630edff084a998ee62f074
JwowSquared <Jdwagner1@hotmail.com>
2020-09-26 19:29:55 +0000

    mindlessly trying to fix this

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -6,6 +6,10 @@ def lazy_matrix_mul(m_a, m_b):
     """Multiply two matrices together with numpy"""
     try:
         import numpy
+        if type(m_a) is not list:
+            raise TypeError("m_a must be a list")
+        if type(m_b) is not list:
+            raise TypeError("m_b must be a list")
         ma = numpy.array(m_a)
         mb = numpy.array(m_b)
         return numpy.array2string(numpy.dot(ma, mb))
