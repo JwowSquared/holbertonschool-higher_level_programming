37a3d6988121b2c492394b55356c8fea83901b79
JwowSquared <Jdwagner1@hotmail.com>
2020-09-26 12:44:10 +0000

    added tests and script for task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module holds the function print_square, for Python 0x07 Task 3"""
+
+
+def print_square(size):
+    """prints a square of #'s based on input size"""
+    if type(size) not in [int, float]:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        print("")
+    try:
+        for i in range(size):
+            print("#" * size)
+    except:
+        raise
