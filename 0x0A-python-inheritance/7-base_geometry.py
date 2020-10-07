5e6b25b88ed8b5813053ad8f3da35b9708b6a8ae
JwowSquared <Jdwagner1@hotmail.com>
2020-10-07 02:51:25 +0000

    added task 7 test cases and script

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""x"""
+
+
+class BaseGeometry:
+    """x"""
+    def area(self):
+        """x"""
+        raise Exception("area() is not implemented")
+
+
+    def integer_validator(self, name, value):
+        """x"""
+        if not isinstance(value, int):
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
