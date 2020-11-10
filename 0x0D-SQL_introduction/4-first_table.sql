3f115568ada62dacfbfe87023b13c0392a3be59a
JwowSquared <Jdwagner1@hotmail.com>
2020-11-10 10:58:51 +0000

    removed all the mumbo jumbo that should 100% be there

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,3 @@
 -- create a table called first_table in the current database
 -- this query creates a table with fields id and name
-IF NOT EXISTS (CREATE TABLE first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256);
