d26876ac666e6e14ac91b3e1a1cfea4c3813545e
JwowSquared <Jdwagner1@hotmail.com>
2020-11-10 11:05:15 +0000

    i found the parenthesis i was missing

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- create a table called first_table in the current database
 -- this query creates a table with fields id and name
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256);
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
