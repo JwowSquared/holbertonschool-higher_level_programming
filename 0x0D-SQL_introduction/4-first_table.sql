125a6f894a356c6539364e0d72423d153a0684a1
JwowSquared <Jdwagner1@hotmail.com>
2020-11-10 10:54:22 +0000

    added task 4 query

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- create a table called first_table in the current database
+-- this query creates a table with fields id and name
+CREATE TABLE IF NOT EXISTS first_table (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+name VARCHAR(256);
