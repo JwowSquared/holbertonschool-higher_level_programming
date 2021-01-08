ba42e613f80fc616b29a64a788728871719f5896
JwowSquared <Jdwagner1@hotmail.com>
2021-01-08 10:45:27 +0000

    added task 2 script

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""x"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    engine = db.cursor()
+
+    engine.execute("SELECT * FROM states")
+    for record in engine.fetchall():
+        if record[1] == argv[4]:
+            print(record)
+
+    engine.close()
+    db.close()
