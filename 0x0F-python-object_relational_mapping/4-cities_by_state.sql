e4f107b2f49aca555918d91578c2dbedfb3cf1af
JwowSquared <Jdwagner1@hotmail.com>
2021-01-08 11:38:18 +0000

    added task 4 script

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.sql
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""x"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    engine = db.cursor()
+
+    engine.execute("""SELECT cities.id, cities.name, states.name FROM states
+                      INNER JOIN cities ON states.id=cities.state_id""")
+    for record in engine.fetchall():
+            print(record)
+
+    engine.close()
+    db.close()
