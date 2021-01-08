198bb6ece31cfbc481fecc902f657e53a6e57b3d
JwowSquared <Jdwagner1@hotmail.com>
2021-01-08 10:58:43 +0000

    changed back to = instead of LIKE keyword, added case check in the output loop instead of bothering trying to get the query to work

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,9 +13,10 @@ if __name__ == "__main__":
                          db=argv[3])
     engine = db.cursor()
 
-    engine.execute("SELECT * FROM states WHERE name LIKE '{}'".format(argv[4]))
+    engine.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
     for record in engine.fetchall():
-        print(record)
+        if record == argv[4]:
+            print(record)
 
     engine.close()
     db.close()
