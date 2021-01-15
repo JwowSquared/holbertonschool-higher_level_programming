c28810eb547af48c6b0c258eafa0736c57047769
JwowSquared <Jdwagner1@hotmail.com>
2021-01-15 16:09:21 +0000

    added task 12 script, i refuse to believe that a .first() is required

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""x"""
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from model_state import Base, State
+
+    engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
+                           .format(argv[1],
+                                   argv[2],
+                                   "localhost",
+                                   argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    factory = sessionmaker(bind=engine, expire_on_commit=False)
+    session = factory()
+
+    out = session.query(State).filter_by(id=2)
+    out.name = "New Mexico"
+    session.commit()
+
+    session.close()
