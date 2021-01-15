ca1dca068c9b4f8039906e39a51fcf6fbc82e275
JwowSquared <Jdwagner1@hotmail.com>
2021-01-15 15:55:07 +0000

    added task 10 script

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""x"""
+
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from model_state import Base, State
+
+    engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
+                           .format(argv[1],
+                                   argv[2],
+                                   "localhost",
+                                   argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    factory = sessionmaker(bind=engine, expire_on_commit=False)
+    session = factory()
+
+    out = "Not found"
+    for record in session.query(State):
+        if argv[4] == record.name:
+            out = "{}: {}".format(record.id, record.name)
+    print(out)
+
+    session.close()
