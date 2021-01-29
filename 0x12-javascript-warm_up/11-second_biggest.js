0605ef9df1d6220d3642ae5a1625e8eaf84e76c3
JwowSquared <Jdwagner1@hotmail.com>
2021-01-29 02:06:21 +0000

    now sorts array and grabs second to last element

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,14 @@
 #!/usr/bin/node
 const args = process.argv;
 const argc = args.length;
-let out, max, i;
+let i, nums = [];
 
 if (argc < 4) {
   console.log(0);
 } else {
-  max = args[2];
-  if (max >= args[3]) {
-    out = args[3];
+  for (i = 2; i < argc; i++) {
+    nums.push(Number(args[i]));
   }
-  for (i = 4; i < argc; i++) {
-    if (max <= args[i]) {
-      out = max;
-      max = args[i];
-    }
-  }
-  console.log(out);
+  nums.sort();
+  console.log(nums[argc - 4]);
 }
