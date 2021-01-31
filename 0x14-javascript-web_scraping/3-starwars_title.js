9b33915d72044e363c3b5ebf337dd5eed86c9c15
JwowSquared <Jdwagner1@hotmail.com>
2021-01-31 11:01:09 +0000

    added task 3 script with comments

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else { // if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  //  } else {
+  //    console.log('code: ' + response.statusCode);
+  }
+});
