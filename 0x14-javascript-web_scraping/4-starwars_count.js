518d9ac1ff452802568b4496551f14ef4faa4768
JwowSquared <Jdwagner1@hotmail.com>
2021-01-31 12:16:54 +0000

    now specifically checks the string if it contains 18 instead of matching the entire string which is arguably more responsible

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,16 +2,18 @@
 
 const request = require('request');
 
-request(process.argv[2], function (err, response, body) {
+request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
     let out = 0;
     const list = JSON.parse(body);
     list.results.forEach(dict => {
-      if (dict.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-        out++;
-      }
+      dict.characters.forEach(character => {
+        if (character.includes('18')) {
+          out++;
+        }
+      });
     });
     console.log(out);
   }
