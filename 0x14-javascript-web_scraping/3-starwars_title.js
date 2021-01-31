53e3d6cb7c6137b9f031265173b1b299aaa51eae
JwowSquared <Jdwagner1@hotmail.com>
2021-01-31 11:02:03 +0000

    removed comments because i was right

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,9 +5,7 @@ const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
-  } else { // if (response.statusCode === 200) {
+  } else {
     console.log(JSON.parse(body).title);
-  //  } else {
-  //    console.log('code: ' + response.statusCode);
   }
 });
