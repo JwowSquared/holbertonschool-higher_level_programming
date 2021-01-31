f8dfa4d9614920c87a6c91595939962b1217d07f
JwowSquared <Jdwagner1@hotmail.com>
2021-01-31 12:25:49 +0000

    removed status code check because i dont need it

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,7 @@ const request = require('request');
 request(process.argv[2], (err, response, body) => {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
+  } else {
     let out = 0;
     const list = JSON.parse(body);
     list.results.forEach(dict => {
