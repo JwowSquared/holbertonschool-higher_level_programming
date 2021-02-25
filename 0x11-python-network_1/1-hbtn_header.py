d4f69b193c9c1a16a3e87343033e0c636c13aedb
JwowSquared <Jdwagner1@hotmail.com>
2021-02-25 21:59:26 +0000

    added task 1 script

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""x"""
+from urllib import request
+from sys import argv
+
+if __name__ == "__main__":
+    with request.urlopen(argv[1]) as response:
+        print(response.headers.get("X-Request-Id"))
